9437d55b5671c75ab8d556ceb13e974cd3bdc4b3
chiemgiabaost <bao2003@my.yorku.ca>
2024-02-27 16:33:35 -0500

    Create login and sign up

---
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from "@clerk/nextjs";
+import { Sign } from "crypto";
+ 
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen"><SignIn /></div>
+  )
+}
+
+export default SignInPage
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,22 @@
+import Topbar from '@/components/topbar'
+import { cn } from '@/lib/utils'
 import React from 'react'
 
 const DashboardLayout = (props: {
-    children: React.ReactNode
-    
-}) => {
+    children: React.ReactNode}) => {
   return (
-    <div>{props.children}</div>
+    
+    <div>
+      <header>
+        <Topbar />
+      </header>
+      <main className={cn(
+        "lg:bg-gray-950 lg:overflow-hidden lg:pl-80 lg:pr-7 lg:py-7 [&:has([is-navbar-minimal])]:lg:pl-20"
+      )}>
+
+      </main>
+      {props.children}
+      </div>
   )
 }
 
